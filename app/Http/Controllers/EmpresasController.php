de47947518572810542cbb10c200ae03f0471b3b
Romulo Sousa <romulosousa27@hotmail.com>
2018-04-14 20:50:19 -0300

    contruct, show, store, update, delete

--- app/Http/Controllers/EmpresasController.php
+++ app/Http/Controllers/EmpresasController.php
@@ -7,18 +7,54 @@ use App\Empresa;
 
 class EmpresasController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     public function index(){
         $empresas = Empresa::all();
         return response()->json($empresas);
     }
 
     public function show($id){
-        $empresa = Empresa::find($id);
+        $$empresas = Empresa::find($id);
         //verificando se o registro é NULL
-        if(!$empresa){
+        if(!$$empresas){
             return response()->json(['message', 'Dados não encontrados'], 404);
         }
 
-        return response()->json($empresa);
+        return response()->json($empresas);
+    }
+
+    public function store(Request $request){
+        $empresas = new Empresa();
+        $empresas->fill($request->all());
+        $empresas->save();
+
+        return response()->json($empresas, 201);
+    }
+
+    public function update(Request $request, $id){
+        $empresas = Empresa::find($id);
+
+        if(!$empresas){
+            return response()->json(['message', 'Dados não encontrados', 404]);
+        }
+
+        $empresas->fill($request->all());
+        $empresas->save();
+
+        return response()->json($empresas);
+    }
+
+    public function destroy($id){
+        $empresas = Empresa::find($id);
+
+        if(!$empresas){
+            return response()->json(['Dados não encontrados'], 404);
+        }
+
+        $empresas->delete();
     }
 }
--- app/Http/Controllers/TrabalhosController.php
+++ app/Http/Controllers/TrabalhosController.php
@@ -9,18 +9,71 @@ use App\Http\Controllers\Controller;
 
 class TrabalhosController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     public function index(){
         $trabalhos = Trabalho::with('empresa')->get();
         return response()->json($trabalhos);
     }
 
     public function show($id){
-        $trabalho = Trabalho::with('empresa')->find($id);
+        $trabalhos = Trabalho::with('empresa')->find($id);
         //verificando se o registro é NULL
-        if(!$trabalho){
+        if(!$trabalhos){
             return response()->json(['message', 'Dados não encontrados', 404]);
         }
 
-        return response()->json($trabalho);
+        return response()->json($trabalhos);
+    }
+
+    public function store(Request $request){
+        $trabalhos = new Trabalho();
+        $trabalhos->fill($request->all());
+        $trabalhos->save();
+
+        return response()->json($trabalhos, 201);
+    }
+
+    public function update(Request $request, $id){
+        $trabalhos = Trabalho::find($id);
+
+        if(!$trabalhos){
+            return response()->json(['message', 'Dados não encontrados'], 404);
+        }
+
+        $trabalhos->fill($request->all());
+        $trabalhos->save();
+
+        return response()->json($trabalhos);
+    
+        /** Retorno padrão do Laravel para endpoint PUT
+            public function update(Request $request, $id){
+                try {
+                    $job = Job::findOrFail($id);
+
+                    $job->fill($request->all());
+                    $job->save();
+
+                    return response()->json($job);
+                } 
+                catch (Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+                    response()->json($e);
+                }
+            }
+         * 
+         */
+    }
+
+    public function destroy($id){
+        $trabalho = Trabalho::find($id);
+
+        if(!$trabalho){
+            return response()->json(['message', 'Dados não encontrados'], 404);
+        }
+
+        $trabalho->delete();
     }
 }
