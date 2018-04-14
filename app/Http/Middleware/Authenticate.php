2b3c53ee25e2385b9d816fb4062ff476c859022d
Romulo Sousa <romulosousa27@hotmail.com>
2018-04-14 20:50:41 -0300

    autenticador

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($this->auth->guest()){
+            return response()->json(['message', 'Não autorizado'], 401) ;
+        }
+
+        return $next($request); 
+    }
+}
