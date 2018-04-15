92ea37f013adc82e7b1ae3c7baee1e3b8067892c
Romulo Sousa <romulosousa27@hotmail.com>
2018-04-14 21:33:42 -0300

    JWT

--- config/app.php
+++ config/app.php
@@ -159,8 +159,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
         //JWT Token
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        
 
     ],
 
@@ -210,8 +212,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        //JWT
         'JWTAuth'   => Tymon\JWTAuthFacades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuthFacades\JWTFactory::class,
+        'JWTFactory' => Tymon\JWTAuthFacades\JWTFactory::class
 
     ],
 
