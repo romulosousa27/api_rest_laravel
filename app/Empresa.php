4afb880c3b7dc138479253df66c34aa157b8906b
Romulo Sousa <romulosousa27@hotmail.com>
2018-04-16 16:39:02 -0300

    autenticador

--- app/Empresa.php
+++ app/Empresa.php
@@ -4,9 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Trabalho;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
-class Empresa extends Model
+class Empresa extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
+    use Authenticatable, CanResetPassword;
+
     protected $fillable = ['nome', 'email', 'website', 'logo', 'senha'];
     protected $hidden = ['senha'];
     protected $dates = ['deleted_at'];
